2c85828cdd194af2b38cb68bb50e3f82fd25c015
Harrison Shockley <hshockley@rocketsoftware.com>
2019-09-30 17:07:26 -0400

    fix(card): more changes to styling

--- packages/react/src/components/Card/stories/default.js
+++ packages/react/src/components/Card/stories/default.js
@@ -13,14 +13,10 @@ import Link from '../../Link';
 import Card from '../../Card';
 import CardFooter from '../../CardFooter';
 import CardContent from '../../CardContent';
-import { iconAdd } from 'carbon-icons';
 
 export default props => (
   <Card {...props}>
-    <CardContent
-      cardTitle="Card Name"
-      cardInfo={['Secondary Information']}
-      cardIcon={iconAdd}>
+    <CardContent cardTitle="Card Name" cardInfo={['Secondary Information']}>
       <OverflowMenu {...props}>
         <OverflowMenuItem {...props} itemText="Stop App" />
         <OverflowMenuItem {...props} itemText="Restart App" />
