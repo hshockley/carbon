5972c0b07ca18aa81e4b417d3a23b0e6a70d58fb
Harrison Shockley <hshockley@rocketsoftware.com>
2019-10-02 13:26:32 -0400

    feat(ButtonGroup): added initial files and code for buttongroup

---
+++ packages/react/src/components/ButtonGroup/ButtonGroup-story.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean, select, text } from '@storybook/addon-knobs';
+import { settings } from '@rocketsoftware/carbon-components';
+import Button from '../ButtonGroup';
+
+const { prefix } = settings;
+
+const kinds = {
+  'Primary button (primary)': 'primary',
+  'Secondary button (secondary)': 'secondary',
+  'Danger button (danger)': 'danger',
+  'Ghost button (ghost)': 'ghost',
+};
+
+const sizes = {
+  Default: 'default',
+  Field: 'field',
+  Small: 'small',
+};
+
+const props = {
+  regular: () => {
+    return {
+      className: 'some-class',
+      kind: select('Button kind (kind)', kinds, 'primary'),
+      disabled: boolean('Disabled (disabled)', false),
+      size: select('Button size (size)', sizes, 'default'),
+      iconDescription: text(
+        'Icon description (iconDescription)',
+        'Button icon'
+      ),
+      onClick: action('onClick'),
+      onFocus: action('onFocus'),
+      small: boolean('Small (small) - Deprecated in favor of `size`', false),
+    };
+  },
+};
+
+Button.displayName = 'Button';
+
+storiesOf('ButtonGroup', module)
+  .addDecorator(withKnobs)
+  .add(
+    'Default',
+    () => {
+      const regularProps = props.regular();
+      return (
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexWrap: 'wrap',
+          }}>
+          <Button {...regularProps} className="some-class">
+            Button
+          </Button>
+          &nbsp;
+          <Button {...regularProps} href="#" className="some-class">
+            Link
+          </Button>
+          &nbsp;
+          <Button {...regularProps} as="p" href="#" className="some-class">
+            Element
+          </Button>
+          &nbsp;
+          <Button
+            {...regularProps}
+            as={CustomLink}
+            href="#"
+            className="some-class">
+            Custom component
+          </Button>
+        </div>
+      );
+    },
+    {
+      info: {
+        text: `
+          Buttons are used to initialize an action, either in the background or
+          foreground of an experience.
+
+          There are several kinds of buttons.
+
+          Primary buttons should be used for the principle call to action
+          on the page.
+
+          Secondary buttons should be used for secondary actions on each page.
+
+          Danger buttons should be used for a negative action (such as Delete) on the page.
+
+          Modify the behavior of the button by changing its event properties.
+
+          Field buttons may be use directly next to an input element, to visually align their heights.
+
+          Small buttons may be used when there is not enough space for a
+          regular sized button. This issue is most found in tables. Small button should have three words
+          or less.
+
+          When words are not enough, icons can be used in buttons to better communicate what the button does. Icons are
+          always paired with text.
+        `,
+      },
+    }
+  );
---
+++ packages/react/src/components/ButtonGroup/ButtonGroup.js
@@ -0,0 +1,73 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+import { settings } from '@rocketsoftware/carbon-components';
+import { ButtonTypes } from '../../prop-types/types';
+
+
+const { prefix } = settings;
+const ButtonGroup = React.forwardRef(function ButtonGroup(
+    {
+        className,
+        disabled,
+        size,
+        kind,
+        href,
+        tabIndex,
+        type,
+        role,
+        ...other},
+        ref){
+
+
+        });
+
+ButtonGroup.PropTypes = {
+/**
+   * Add an optional class to the button
+   */
+  className: PropTypes.string,
+
+  /**
+   * For specifying whether the button is disabled
+   */
+  disabled: PropTypes.bool,
+
+  /**
+   * Size of button.
+   */
+  size: PropTypes.oneOf(['default', 'field', 'small']),
+
+  /**
+   * Kind of button
+   */
+  kind: ButtonTypes.buttonKind.isRequired,
+
+  /**
+   * Optionally specify an href for your Button to become an <a> element
+   */
+  href: PropTypes.string,
+
+  /**
+   * Optional prop to specify the tabIndex of the Button
+   */
+  tabIndex: PropTypes.number,
+
+  /**
+   * Optional prop to specify the type of the Button
+   */
+  type: PropTypes.oneOf(['button', 'reset', 'submit']),
+
+  /**
+   * Optional prop to specify the role of the Button
+   */
+  role: PropTypes.string,
+  };
+  ButtonGroup.defaultProps = {
+    tabIndex: 0,
+    type: 'button',
+    disabled: false,
+    kind: 'primary',
+  };
+  
+  export default ButtonGroup;
