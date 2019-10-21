0e3911fc361b94c022a6f9b9a3b1d566ab3e39c0
Harrison Shockley <hshockley@rocketsoftware.com>
2019-10-21 16:12:59 -0400

    refactor(split-button): added initial tests

--- packages/react/src/components/SplitButton/SplitButton-test.js
+++ packages/react/src/components/SplitButton/SplitButton-test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import SplitButton from '../SplitButton';
 import OverflowMenuItem from '../OverflowMenuItem';
+import Button from '../Button';
+import OverflowMenu from '../OverflowMenu';
 import { shallow } from 'enzyme';
 
-describe('TabContent', () => {
+describe('SplitButton', () => {
   describe('renders as expected', () => {
     const wrapper = shallow(
-      <SplitButton>
+      <SplitButton
+        classNameContainer={'extra-class'}
+        classNameButton={'extra-class'}
+        classNameOverflow={'extra-class'}>
         <OverflowMenuItem itemText={'Item 1'} />
         <OverflowMenuItem itemText={'Item 2'} primaryFocus />
         <OverflowMenuItem itemText={'Item 2'} />
@@ -17,14 +22,20 @@ describe('TabContent', () => {
       expect(wrapper.props().children.length).toEqual(2);
     });
 
-    it('sets selected if passed in via props', () => {
-      wrapper.setProps({ selected: true });
-      expect(wrapper.props().selected).toEqual(true);
-    });
-
-    it('sets selected and hidden props with opposite boolean values', () => {
-      wrapper.setProps({ selected: true });
-      expect(wrapper.props().hidden).toEqual(false);
+    it('renders extra classes as expected', () => {
+      expect(wrapper.hasClass('extra-class')).toEqual(true);
+      expect(
+        wrapper
+          .find(Button)
+          .shallow()
+          .hasClass('extra-class')
+      ).toEqual(true);
+      expect(
+        wrapper
+          .find(OverflowMenu)
+          .shallow()
+          .hasClass('extra-class')
+      ).toEqual(true);
     });
   });
 });
