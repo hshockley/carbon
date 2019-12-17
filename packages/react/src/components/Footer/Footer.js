c13ea8897618c267d207252aee6cac16ec8d0043
Harrison Shockley <hshockley@rocketsoftware.com>
2019-12-17 18:04:37 -0500

    feat(footer): initial commit

---
+++ packages/react/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classnames from 'classnames';
+import { settings } from '@rocketsoftware/carbon-components';
+
+const { prefix } = settings;
+
+const Footer = ({ }) => {
+
+
+    return (
+        <h1>gib gob gab golab</h1>
+    );
+};
+
+export default Footer
